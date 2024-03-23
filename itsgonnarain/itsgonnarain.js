9cdd498ed91c54b57466e5319e104287de2050da
Nick <maniutin@gmail.com>
2024-03-23 15:19:01 -0400

    move loop start to a separate function

--- itsgonnarain/itsgonnarain.js
+++ itsgonnarain/itsgonnarain.js
@@ -1,17 +1,22 @@
 let audioContext = new AudioContext();
 
+function startLoop(audioBuffer) {
+  let sourceNode = audioContext.createBufferSource();
+  sourceNode.buffer = audioBuffer;
+  sourceNode.loop = true;
+  sourceNode.loopStart = 2.8;
+  sourceNode.loopEnd = 3.6;
+  sourceNode.connect(audioContext.destination);
+  sourceNode.start(0, 3);
+}
+
 fetch("itsgonnarain.wav")
   .then((response) => response.arrayBuffer())
   .then((arrayBuffer) => audioContext.decodeAudioData(arrayBuffer))
   .then((audioBuffer) => {
-    let sourceNode = audioContext.createBufferSource();
-    sourceNode.buffer = audioBuffer;
-    sourceNode.loop = true;
-    sourceNode.loopStart = 2.8;
-    sourceNode.loopEnd = 3.6;
-    sourceNode.connect(audioContext.destination);
     $("#start").click(function () {
-      sourceNode.start(0, 3);
+      startLoop(audioBuffer);
+      startLoop(audioBuffer);
     });
   })
   .catch((e) => console.error(e));
