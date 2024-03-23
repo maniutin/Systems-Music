b9f945d51fdf16a8f3406deef7139658cbd4dab4
Nick <maniutin@gmail.com>
2024-03-23 15:04:52 -0400

    add file decoding

--- itsgonnarain/itsgonnarain.js
+++ itsgonnarain/itsgonnarain.js
@@ -1,4 +1,7 @@
+let audioContext = new AudioContext();
+
 fetch("itsgonnarain.mp3")
   .then((response) => response.arrayBuffer())
-  .then((arrayBuffer) => console.log("Received", arrayBuffer))
+  .then((arrayBuffer) => audioContext.decodeAudioData(arrayBuffer))
+  .then((audioBuffer) => console.log("Decoded", audioBuffer))
   .catch((e) => console.error(e));
