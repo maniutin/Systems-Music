2b441d40938d0b7871b46b64580c25e544195680
Nick <maniutin@gmail.com>
2024-03-29 15:00:21 -0400

    trigger loops at varying intervals

--- musicforairports/musicforairports.js
+++ musicforairports/musicforairports.js
@@ -179,5 +179,11 @@ fetchSample("AirportTerminal.wav").then((convolverBuffer) => {
   convolver.buffer = convolverBuffer;
   convolver.connect(audioContext.destination);
 
-  startLoop("Grand Piano", "C4", convolver, 20, 5);
+  startLoop("Grand Piano", "F4", convolver, 19.7, 4.0);
+  startLoop("Grand Piano", "Ab4", convolver, 17.8, 8.1);
+  startLoop("Grand Piano", "C5", convolver, 21.3, 5.6);
+  startLoop("Grand Piano", "Db5", convolver, 22.1, 12.6);
+  startLoop("Grand Piano", "Eb5", convolver, 18.4, 9.2);
+  startLoop("Grand Piano", "F5", convolver, 20.0, 14.1);
+  startLoop("Grand Piano", "Ab5", convolver, 17.7, 3.1);
 });
