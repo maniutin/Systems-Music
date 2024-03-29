66c07eb56592a9866f026e8c9eb5f28b8d2abcb3
Nick <maniutin@gmail.com>
2024-03-29 13:35:20 -0400

    add audio context, fetch samples and adjust playback rate

--- musicforairports/musicforairports.js
+++ musicforairports/musicforairports.js
@@ -82,6 +82,24 @@ function noteValue(note, octave) {
   return octave * 12 + OCTAVE.indexOf(note);
 }
 
+function getNoteDistance(note1, octave1, note2, octave2) {
+  return noteValue(note1, octave1) - noteValue(note2 - octave2);
+}
+
+function getNearestSample(sampleBank, note, octave) {
+  let sortedBank = sampleBank.slice().sort((sampleA, sampleB) => {
+    let distanceToA = Math.abs(
+      getNoteDistance(note, octave, sampleA.note, sampleA.octave)
+    );
+    let distanceToB = Math.abs(
+      getNoteDistance(note, octave, sampleB.note, sampleB.octave)
+    );
+
+    return distanceToA - distanceToB;
+  });
+  return sortedBank[0];
+}
+
 function flatToSharp(note) {
   switch (note) {
     case "Bb":
@@ -104,4 +122,39 @@ function getSample(instrument, noteAndOctave) {
     noteAndOctave
   );
   requestedOctave = parseInt(requestedOctave, 10);
+  requestedNote = flatToSharp(requestedNote);
+
+  let sampleBank = SAMPLE_LIBRARY[instrument];
+  let sample = getNearestSample(sampleBank, requestedNote, requestedOctave);
+  let distance = getNoteDistance(
+    requestedNote,
+    requestedOctave,
+    sample.note,
+    sample.octave
+  );
+
+  return fetchSample(sample.file).then((audioBuffer) => ({
+    audioBuffer,
+    distance,
+  }));
+}
+
+let audioContext = new AudioContext();
+
+function fetchSample(path) {
+  return fetch(encodeURIComponent(path))
+    .then((response) => response.arrayBuffer())
+    .then((arrayBuffer) => audioContext.decodeAudioData(arrayBuffer));
+}
+
+function playSample(instrument, note) {
+  getSample(instrument, note).then(({ audioBuffer, distance }) => {
+    let playbackRate = Math.pow(2, distance / 12);
+    let bufferSource = audioContext.createBufferSource();
+
+    bufferSource.buffer = audioBuffer;
+    bufferSource.playbackRate.value = playbackRate;
+    bufferSource.connect(audioContext.destination);
+    bufferSource.start();
+  });
 }
