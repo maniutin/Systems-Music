b9671d7f9095302f27ae90cc9e7de89535678782
Nick <maniutin@gmail.com>
2024-04-05 14:56:56 -0400

    refactor synth creation into a function

--- discreetmusic/discreetmusic.js
+++ discreetmusic/discreetmusic.js
@@ -1,37 +1,41 @@
 Tone.Transport.bpm.value = 240;
-let envelope = {
-  attack: 0.1,
-  release: 4,
-  releaseCurve: "linear",
-};
 
-let filterEnvelope = {
-  baseFrequency: 200,
-  octaves: 2,
-  attack: 0,
-  decay: 0,
-  release: 1000,
-};
+function makeSynth() {
+  let envelope = {
+    attack: 0.1,
+    release: 4,
+    releaseCurve: "linear",
+  };
 
-let synth = new Tone.DuoSynth({
-  harmonicity: 1,
-  voice0: {
-    oscillator: { type: "sawtooth" },
-    envelope,
-    filterEnvelope,
-  },
-  voice1: {
-    oscillator: { type: "sine" },
-    envelope,
-    filterEnvelope,
-  },
-  vibratoRate: 0.5,
-  vibratoAmount: 0.1,
-});
-synth.toMaster();
+  let filterEnvelope = {
+    baseFrequency: 200,
+    octaves: 2,
+    attack: 0,
+    decay: 0,
+    release: 1000,
+  };
+  return new Tone.DuoSynth({
+    harmonicity: 1,
+    voice0: {
+      oscillator: { type: "sawtooth" },
+      envelope,
+      filterEnvelope,
+    },
+    voice1: {
+      oscillator: { type: "sine" },
+      envelope,
+      filterEnvelope,
+    },
+    vibratoRate: 0.5,
+    vibratoAmount: 0.1,
+  });
+}
 
-new Tone.Loop((time) => {
-  synth.triggerAttackRelease("C4", "8n", "+4n");
-}, "1m").start();
+let leftSynth = makeSynth();
+let rightSynth = makeSynth();
 
-Tone.Transport.start();
+let leftPanner = new Tone.Panner(-0.5).toMaster();
+let rightPanner = new Tone.Panner(0.5).toMaster();
+
+leftSynth.connect(leftPanner);
+rightSynth.connect(rightPanner);
