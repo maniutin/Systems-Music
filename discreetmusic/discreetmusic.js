b153535cec9a62d4b3d5927054871e6dd29f2b99
Nick <maniutin@gmail.com>
2024-04-05 11:25:00 -0400

    add amp attack and release

--- discreetmusic/discreetmusic.js
+++ discreetmusic/discreetmusic.js
@@ -1,5 +1,10 @@
 let synth = new Tone.MonoSynth({
   oscillator: { type: "sawtooth" },
+  envelope: {
+    attack: 0.1,
+    release: 4,
+    releaseCurve: "linear",
+  },
   filterEnvelope: {
     baseFrequency: 200,
     octaves: 2,
