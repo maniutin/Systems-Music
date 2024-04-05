91344941333f9e763d30ccc198d949f2773ba4ad
Nick <maniutin@gmail.com>
2024-04-05 11:28:34 -0400

    add DuoSynth

--- discreetmusic/discreetmusic.js
+++ discreetmusic/discreetmusic.js
@@ -1,15 +1,33 @@
-let synth = new Tone.MonoSynth({
-  oscillator: { type: "sawtooth" },
-  envelope: {
-    attack: 0.1,
-    release: 4,
-    releaseCurve: "linear",
+let synth = new Tone.DuoSynth({
+  voice0: {
+    oscillator: { type: "sawtooth" },
+    envelope: {
+      attack: 0.1,
+      release: 4,
+      releaseCurve: "linear",
+    },
+    filterEnvelope: {
+      baseFrequency: 200,
+      octaves: 2,
+      attack: 0,
+      decay: 0,
+      release: 1000,
+    },
   },
-  filterEnvelope: {
-    baseFrequency: 200,
-    octaves: 2,
-    attack: 0,
-    decay: 0,
+  voice1: {
+    oscillator: { type: "sine" },
+    envelope: {
+      attack: 0.1,
+      release: 4,
+      releaseCurve: "linear",
+    },
+    filterEnvelope: {
+      baseFrequency: 200,
+      octaves: 2,
+      attack: 0,
+      decay: 0,
+      release: 1000,
+    },
   },
 });
 synth.toMaster();
